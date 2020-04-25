03afdbaf51d2986065d5f12a518ffd1a781ba33c
tjhume <tristanjhume@gmail.com>
2020-04-25 13:54:37 -0600

    Fixed typo

--- build-win.bat
+++ build-win.bat
@@ -1,1 +1,1 @@
-electron-packager . "TJ's Tack Tracker" --overwrite --platform=win32 --arch=ia32 --icon=src/assets/img/tj-win.ico --prune=true --out=release-builds --version-string.CompanyName=CE --version-string.FileDescription=CE --version-string.ProductName="TJ's Task Tracker"
+electron-packager . "TJ's Task Tracker" --overwrite --platform=win32 --arch=ia32 --icon=src/assets/img/tj-win.ico --prune=true --out=release-builds --version-string.CompanyName=CE --version-string.FileDescription=CE --version-string.ProductName="TJ's Task Tracker"
